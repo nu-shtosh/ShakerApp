2be7dfad4f24ec067c2915cd7eb600075ca9c949
ILIA <dubenskiiilia@gmail.com>
2022-12-26 01:33:13 +0100

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Илья Дубенский on 26.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
