85eaa72153708e8128528845d227f16963980542
ILIA <dubenskiiilia@gmail.com>
2022-12-27 01:42:55 +0100

    some marks

--- Drawing/Views/ContentView.swift
+++ Drawing/Views/ContentView.swift
@@ -24,53 +24,62 @@ struct ContentView: View {
             BackgroundView()
 
             VStack {
-                Text("1. Choose colors and add in the shaker")
+                VStack {
+                    Text("1. Choose colors and add them in the shaker")
+                        .foregroundColor(.white)
+                        .bold()
+                    HStack {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 100, height: 100)
+                                .background(Color.clear)
+                                .foregroundColor(Color.clear)
+                            RectangleView(
+                                redColor: redSlider,
+                                greenColor: greenSlider,
+                                blueColor: blueSlider
+                            )
+                            .frame(width: addColorInShaker ? 100 : 10,
+                                   height: addColorInShaker ? 100 : 10)
+                            .offset(
+                                x: addColorInShaker ? 0 : UIScreen.main.bounds.width - 230,
+                                y: addColorInShaker ? 0 : UIScreen.main.bounds.height - 570
+                            )
+                            .animation(
+                                Animation.easeIn(duration: 0.6).repeatCount(0, autoreverses: true),
+                                value: addColorInShaker)
+                        }
+                        VStack {
+                            SliderView(value: $redSlider, color: .red)
+                            SliderView(value: $greenSlider, color: .green)
+                            SliderView(value: $blueSlider, color: .blue)
+                        }
+                    }
+                    .padding()
+
+                    Button("Add color in Shaker") {
+                        addColor()
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            addColorInShaker = true
+                        }
+                    }
                     .foregroundColor(.white)
-                HStack {
-                    ZStack {
-                        Rectangle()
-                            .frame(width: 100, height: 100)
-                            .background(Color.clear)
-                            .foregroundColor(Color.clear)
-                        RectangleView(
-                            redColor: redSlider,
-                            greenColor: greenSlider,
-                            blueColor: blueSlider
-                        )
-                        .frame(width: addColorInShaker ? 100 : 10,
-                               height: addColorInShaker ? 100 : 10)
-                        .offset(
-                            x: addColorInShaker ? 0 : UIScreen.main.bounds.width - 230,
-                            y: addColorInShaker ? 0 : UIScreen.main.bounds.height - 570
-                        )
-                        .animation(
-                            Animation.easeIn(duration: 0.6).repeatCount(0, autoreverses: true),
-                            value: addColorInShaker)
-                    }
-                    VStack {
-                        SliderView(value: $redSlider, color: .red)
-                        SliderView(value: $greenSlider, color: .green)
-                        SliderView(value: $blueSlider, color: .blue)
-                    }
+                    .frame(width: 250, height: 50)
+                    .background(Color.orange)
+                    .cornerRadius(12)
                 }
                 .padding()
+                .background(Color(.white).opacity(0.2))
+                .cornerRadius(10)
 
-                Button("Add color in Shaker") {
-                    addColor()
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                        addColorInShaker = true
-                    }
-                }
-                .foregroundColor(.white)
-                .frame(width: 250, height: 50)
-                .background(Color.orange)
-                .cornerRadius(12)
 
                 Spacer()
+
                 Text("2. Shake it")
                     .foregroundColor(.white)
-                HStack {
+                    .bold()
 
+                HStack {
                     VStack {
                         GlassView(
                             colors: !shake ? [.white.opacity(0.1)] : addedColors,
@@ -106,22 +115,28 @@ struct ContentView: View {
                     .offset(y: shake ? 50 : 0)
                     .rotationEffect(.degrees(shake ? -45 : 0))
                     .animation(Animation.easeInOut(duration: 0.1).repeatCount(10, autoreverses: true), value: shake)
-//                    Spacer()
                 }
+
+                Text("Colors in shaker: \(addedColors.count)")
+                    .foregroundColor(.cyan)
 
                 Button("Shake") {
                     print(addedColors)
                     shakeColors()
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
                         shake = false
                     }
                 }
+                .disabled(addedColors.isEmpty)
                 .foregroundColor(.white)
                 .frame(width: 250, height: 50)
-                .background(Color.orange)
+                .background(addedColors.isEmpty ? Color.gray : Color.green)
                 .cornerRadius(12)
+
                 Text("3. Clear it and repeat")
                     .foregroundColor(.white)
+                    .bold()
+
                 Button("Clear") {
                     clearGlass.toggle()
                     addedColors.removeAll()
@@ -131,7 +146,7 @@ struct ContentView: View {
                 }
                 .foregroundColor(.white)
                 .frame(width: 250, height: 50)
-                .background(Color.orange)
+                .background(Color.red)
                 .cornerRadius(12)
             }
             .padding()
